df135ee915591a31891bf138e85cfdf014cf5a50
Bruno Henrique <brunohbm23@gmail.com>
2026-05-01 21:06:20 -0300

    study

--- src/app/components/spells-menu/spells-menu.component.ts
+++ src/app/components/spells-menu/spells-menu.component.ts
@@ -8,7 +8,6 @@ import ISpell from '../../shared/classes/spells/ISpell';
 import { AudioService } from '../../shared/services/audio/audio.service';
 import AUDIO_PATHS from '../../shared/services/audio/audio-paths';
 import { SpellPopoverDirective } from '../../shared/directives/spell-popover.directive';
-import { ItemPopoverDirective } from '../../shared/directives/item-popover.directive';
 
 @Component({
   selector: 'app-spells-menu',
